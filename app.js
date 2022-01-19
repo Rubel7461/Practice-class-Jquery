69894be158047817b4550ca426e7311e0f90da89
Rubel7461 <developerrubel.me@gmail.com>
2022-01-19 17:22:12 +0600

    Uploads new app.js file

---
+++ app.js
@@ -0,0 +1,5 @@
+
+var names=prompt('Please type your name here');
+var hobby=prompt('Please type your hobby name here');
+
+console.log('I am a professional wordpress developer and woocommerce specialist. My name is '+ names +'. My favorites hobby is '+ hobby +' ');
